d46298d469c0797c9df978da1b05b999f46ce11b
Micah Ives <micbenives@gmail.com>
2023-10-09 22:02:41 -0500

    Update search button, add event listener

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
-const apiKey = d3d82e6a2d8834ff83e6cab9c08ac5c2;
+const apiKey = 'd3d82e6a2d8834ff83e6cab9c08ac5c2';
+var weatherContainer = document.getElementById('weather-container');
+var searchButton = document.getElementById('search-button');
 
-fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`)
-    .then(response => response.json())
-    .then(data => {
-        const weatherInfo = document.getElementById('weather-info');
-        const firstForecast = data.list[0];
-        const temperature = firstForecast.main.temp;
-        const description = firstForecast.weather[0].description;
-        const html = `<p>Temperature: ${temperature}°C</p><p>Description: ${description}</p>`;
-        weatherInfo.innerHTML = html;
-})
-.catch(error => {
-    console.error('Error fetching weather data:', error);
-});
+function getApi() {
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`)
+        .then(response => response.json())
+        .then(data => {
+            for (var i =0; i < data.length; i++) {
+                const weatherInfo = document.getElementById('weather-info');
+                const firstForecast = data.list[0];
+                const temperature = firstForecast.main.temp;
+                const description = firstForecast.weather[0].description;
+                const html = `<p>Temperature: ${temperature}°C</p><p>Description: ${description}</p>`;
+                weatherInfo.innerHTML = html;
+                console.log("success!")
+            }
+    })
+        .catch(error => {
+            console.error('Error fetching weather data:', error);
+    });
+}
+
+searchButton.addEventListener('click', getApi);
