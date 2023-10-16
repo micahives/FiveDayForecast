fcc62f2a3ec723f42249ab1e55b9bbc3d0ed6728
Micah Ives <micbenives@gmail.com>
2023-10-16 16:45:08 -0500

    Update fetch api and render JSON functions

--- script.js
+++ script.js
@@ -1,30 +1,41 @@
-var weatherContainer = document.getElementById('weather-container');
 var searchButton = document.getElementById('search-button');
 
-function getWeather() {
-  console.log("Hello!");
-  const apiKey = 'd3d82e6a2d8834ff83e6cab9c08ac5c2';
-  const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?appid=${apiKey}`;
+function renderWeather(weather) {
+  var weatherContainer = document.querySelector("#weather-container");
+  console.log(weather);
+
+  // Create h2 for city name
+  // Create p for temp, humidity, wind, description
+  var city = document.createElement("h2");
+  city.textContent = weather.name;
+  weatherContainer.append(city);
+  
+  var temp = document.createElement('p');
+  temp.textContent = "Temp: " + weather.main.temp + " F";
+  weatherContainer.append(temp);
+
+  var humidity = document.createElement('p');
+  humidity.textContent = "%RH: " + weather.main.humidity + " %";
+  weatherContainer.append(humidity);
+
+  var wind = document.createElement('p');
+  wind.textContent = "Wind: " + weather.wind.speed + " mph, " + weather.wind.deg + " deg";
+  weatherContainer.append(wind);
+
+}
+
+// Fetch weather data for city
+function fetchWeather(query) {
+  var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + query + '&units=imperial&appid=d3d82e6a2d8834ff83e6cab9c08ac5c2';
+
   fetch(apiUrl)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      console.log(data);
-      // for (var i = 0; i < data.length; i++) {
-      //   //Creating a h3 element and a p element
-      //   var userName = document.createElement('h3');
-      //   var userUrl = document.createElement('p');
+      renderWeather(data);
+    });
 
-      //   //Setting the text of the h3 element and p element.
-      //   userName.textContent = data[i].login;
-      //   userUrl.textContent = data[i].url;
+}
 
-      //   //Appending the dynamically generated html to the div associated with the id="users"
-      //   //Append will attach the element as the bottom most child.
-      //   usersContainer.append(userName);
-      //   usersContainer.append(userUrl);
-      });
-};
-
-searchButton.addEventListener('click', getWeather);
+fetchWeather();
