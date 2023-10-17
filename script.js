0db764c5ab4f81d8db7803488e8d1011dca1d927
Micah Ives <micbenives@gmail.com>
2023-10-17 17:28:01 -0500

    Add current weather fetch and forecast fetch, update render functions

--- script.js
+++ script.js
@@ -1,41 +1,85 @@
 var searchButton = document.getElementById('search-button');
 
-function renderWeather(weather) {
+function renderCurrentWeather(weather) {
   var weatherContainer = document.querySelector("#weather-container");
   console.log(weather);
 
   // Create h2 for city name
-  // Create p for temp, humidity, wind, description
   var city = document.createElement("h2");
   city.textContent = weather.name;
   weatherContainer.append(city);
+
+  // Icon
+  var icon = document.createElement("img");
+  var iconObj = weather.weather[0].icon;
+  var iconURL = `https://openweathermap.org/img/wn/${iconObj}@2x.png`;
+  icon.setAttribute('src', iconURL);
+  weatherContainer.append(icon);
+
+  // Date <p>
+  var date = document.createElement('p');
+  var newDate = new Date(weather.dt * 1000).toLocaleDateString("en-US");
+  date.textContent = "Date: " + newDate;
+  weatherContainer.append(date);
   
+  // Temp <p>
   var temp = document.createElement('p');
-  temp.textContent = "Temp: " + weather.main.temp + " F";
+  temp.textContent = "Temp: " + weather.main.temp + " °F";
   weatherContainer.append(temp);
 
+  // Humidity <p>
   var humidity = document.createElement('p');
-  humidity.textContent = "%RH: " + weather.main.humidity + " %";
+  humidity.textContent = "Humidity: " + weather.main.humidity + "%";
   weatherContainer.append(humidity);
 
+  // Wind <p>
   var wind = document.createElement('p');
-  wind.textContent = "Wind: " + weather.wind.speed + " mph, " + weather.wind.deg + " deg";
+  wind.textContent = "Wind: " + weather.wind.speed + " mph";
   weatherContainer.append(wind);
 
 }
 
+function renderForecastWeather(weather) {
+  console.log(weather);
+  for (var i = 0; i < 5; i ++) {
+    var forecastContainer = document.querySelector("#forecast-container");
+    var weatherDiv = document.createElement('div');
+    var city = document.createElement("h2");
+    city.textContent = weather.list[i * 8].main.temp;
+    weatherDiv.append(city);
+    forecastContainer.append(weatherDiv);
+  }
+}
+
 // Fetch weather data for city
-function fetchWeather(query) {
-  var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + query + '&units=imperial&appid=d3d82e6a2d8834ff83e6cab9c08ac5c2';
+function fetchForecastWeather(query) {
+  var apiKey = 'd3d82e6a2d8834ff83e6cab9c08ac5c2'
+  var apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=` + query + `&units=imperial&appid=${apiKey}`;
 
   fetch(apiUrl)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      renderWeather(data);
+      renderForecastWeather(data);
     });
 
 }
 
-fetchWeather();
+function fetchCurrentWeather(query) {
+  var apiKey = 'd3d82e6a2d8834ff83e6cab9c08ac5c2'
+  var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=` + query + `&units=imperial&appid=${apiKey}`;
+
+  fetch(apiUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      renderCurrentWeather(data);
+      fetchForecastWeather(query);
+    });
+
+}
+
+fetchCurrentWeather("Madison");
+// fetchWeather("Madison");
