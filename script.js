08000d8513abcbb70282b1e784ca98d4e71a87e2
Micah Ives <micbenives@gmail.com>
2023-10-17 21:11:33 -0500

    Add event handler for user search, update comments

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-var searchButton = document.getElementById('search-button');
-
+// Renders the current weather data as a card
 function renderCurrentWeather(weather) {
   var weatherContainer = document.querySelector("#weather-container");
   console.log(weather);
@@ -19,7 +18,7 @@ function renderCurrentWeather(weather) {
   // Date <p>
   var date = document.createElement('p');
   var newDate = new Date(weather.dt * 1000).toLocaleDateString("en-US");
-  date.textContent = "Date: " + newDate;
+  date.textContent = newDate;
   weatherContainer.append(date);
   
   // Temp <p>
@@ -39,12 +38,13 @@ function renderCurrentWeather(weather) {
 
 }
 
+// Renders the forecast data as cards 
 function renderForecastWeather(weather) {
   console.log(weather);
   for (var i = 0; i < 5; i ++) {
     var forecastContainer = document.querySelector("#forecast-container");
     var weatherDiv = document.createElement('div');
-    weatherDiv.setAttribute('class', 'col-4 border border-black rounded m-5 p-3 fixed-size-div');
+    weatherDiv.setAttribute('class', 'col-lg-2 col-md-4 col-sm-3 border border-black rounded m-4 p-3 fixed-size-div');
 
     // Get the date
     var date = document.createElement('p');
@@ -70,13 +70,12 @@ function renderForecastWeather(weather) {
     var wind = document.createElement('p');
     wind.textContent = "Wind: " + weather.list[i * 8].wind.speed + " mph";
 
-
     weatherDiv.append(date, icon, temp, relHumid, wind);
     forecastContainer.append(weatherDiv);
   }
 }
 
-// Fetch weather data for city
+// Fetches five day weather forecast
 function fetchForecastWeather(query) {
   var apiKey = 'd3d82e6a2d8834ff83e6cab9c08ac5c2'
   var apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=` + query + `&units=imperial&appid=${apiKey}`;
@@ -91,6 +90,7 @@ function fetchForecastWeather(query) {
 
 }
 
+// Fetches current weather data
 function fetchCurrentWeather(query) {
   var apiKey = 'd3d82e6a2d8834ff83e6cab9c08ac5c2'
   var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=` + query + `&units=imperial&appid=${apiKey}`;
@@ -106,5 +106,35 @@ function fetchCurrentWeather(query) {
 
 }
 
-fetchCurrentWeather("Madison");
-// fetchWeather("Madison");
+// Removes child HTML elements from weatherContainer when a new search is performed
+function clearWeatherContainer() {
+  var weatherContainer = document.querySelector("#weather-container");
+
+  if (weatherContainer.children.length > 0) {
+    weatherContainer.innerHTML = '';
+  }
+}
+
+// Removes child HTML elements from forecastContainer when a new search is performed
+function clearForecastContainer() {
+  var forecastContainer = document.querySelector('#forecast-container');
+
+    if (forecastContainer.children.length > 0) {
+      forecastContainer.innerHTML = '';
+    }
+}
+
+// Event handler for search form
+document.getElementById('search-form').addEventListener('submit', function(event) {
+  event.preventDefault();
+  
+  var cityName = document.getElementById('search-input').value;
+
+  if (cityName) {
+      clearWeatherContainer();
+      clearForecastContainer();
+      fetchCurrentWeather(cityName);
+  } else {
+      alert('Please enter a city name');
+  }
+});
